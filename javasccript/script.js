ce58265b38608d0c34505cc8dd58f74c1f3fec96
Cauan Zelazowski-LINUX <61758981+Cauanz@users.noreply.github.com>
2022-11-27 19:52:27 -0300

    variaveis, e uma coisa de manipulacao do dom

--- javasccript/script.js
+++ javasccript/script.js
@@ -1,2 +1,29 @@
-console.log('Hello, World!')
-alert('Hello World!')
+let nome = "Cauan";//STRING
+let idade = 17;//NUMBER
+let sentado = true;//BOOLEAN
+let array = ["ovo", "farinha", "leite", "chocolate"];//ARRAY
+let objeto = {nome: "cauan", idade: "17", cidade: "arapoti"};//OBJECT
+
+console.log(objeto.cidade);
+console.log(typeof objeto)
+
+//COMENTARIO DE 1 LINHA
+
+/*comentario
+de 
+mais
+de
+uma
+linha*/
+
+
+
+/* FORA DO FOCO DA AULA, COISAS EXTERNAS*/
+let escrita = document.getElementById("h1");
+if (objeto.cidade = "arapoti") {
+    escrita.innerHTML = "Arapoti";
+}
+
+let hoje = new Date();
+hoje.getSeconds();
+escrita.innerHTML = hoje.getSeconds();
