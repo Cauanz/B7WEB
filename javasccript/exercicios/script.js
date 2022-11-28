c27bb60663a5907993cdac84a027aedd00daf3a8
Cauan Zelazowski-LINUX <61758981+Cauanz@users.noreply.github.com>
2022-11-27 21:23:07 -0300

    exercicios basicos

---
+++ javasccript/exercicios/script.js
@@ -0,0 +1,12 @@
+//1. Crie uma variavel para armazenar o nome do carro e ponha o valor ferrari. e exiba-a
+let nomecar = "ferrari";
+console.log(nomecar);
+
+
+//2. Crie uma variavel para armazenar o preço do bolo, que cursta R$19,90.e exiba-a
+let preçobolo = 19.90;
+console.log("R$" + preçobolo);
+
+//corrija o codigo a seguir (e exiba-o). let "cidade" = São Paulo
+let cidade = "São Paulo";
+console.log(cidade)
